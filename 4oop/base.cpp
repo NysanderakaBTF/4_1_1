0d4b68f4506df8da1cf9198bbf36d2b63ab95a63
student <student@VT-05-434002973>
2022-03-29 12:02:00 +0300

    начало КЛ_3_1

--- 4oop/base.cpp
+++ 4oop/base.cpp
@@ -2,23 +2,24 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-base::base(base* h, std::string n)
+base::base(base* h, std::string n, int stat)
 {
-	this->head = h;
+	head = h;
 	name = n;
-	if (this->head != nullptr) {
-		this->head->ar_p.emplace(this->head->ar_p.end(),this);
+	status = stat;
+	if (head != nullptr) {
+		head->ar_p.emplace(head->ar_p.end(), this);
 	}
 }
 
 void base::set_name(std::string n)
 {
-	this->name = n;
+	name = n;
 }
 
 std::string base::get_name()
 {
-	return this->name;
+	return name;
 }
 
 void base::display()
@@ -32,61 +33,65 @@ void base::display()
 	while (now->ar_p.size() != 0) {
 		std::cout << now->name;
 		for (int i = 0; i < now->ar_p.size(); i++) {
-			std::cout<< " " << now->ar_p[i]->get_name() ;
+			std::cout << "  " << now->ar_p[i]->get_name();
 		}
 		now = now->ar_p[now->ar_p.size() - 1];
-		if(now->ar_p.size() != 0)
+		if (now->ar_p.size() != 0)
 			std::cout << std::endl;
 	}
 }
 
-void base::rebase(base* new_head)
+void base::rebase(base* new_per)
 {
-	bool is_in_tre = false;
-
-	base* now = new_head;
-	while (now->get_head_p() != nullptr) {
-		now = now->get_head_p();
+	if (head != nullptr) {
+		head->ar_p.erase(find(head->ar_p.begin(), head->ar_p.end(), this));
 	}
-	while (now->ar_p.size() != 0) {
-		if (find(now->ar_p.begin(), now->ar_p.end(), this) == now->ar_p.end())
-			is_in_tre = false;
-		else {
-			is_in_tre = true;
-			break;
-		}
-		now = now->ar_p[now->ar_p.size() - 1];
+	if (new_per != nullptr) {
+		new_per->ar_p.emplace_back(this);
 	}
-
-	if (this->get_head_p() != nullptr && new_head != nullptr) {
-		this->get_head_p()->ar_p.erase(find(this->get_head_p()->ar_p.begin(), this->get_head_p()->ar_p.end(),this));
-		this->head = new_head;
-		new_head->ar_p.emplace(new_head->ar_p.begin(),this);
-	}
-	else if (this->get_head_p() == nullptr && is_in_tre) {
-		new_head->get_head_p()->ar_p.erase(find(new_head->get_head_p()->ar_p.begin(), new_head->get_head_p()->ar_p.end(), new_head));
-		this->head = new_head;
-		new_head->head = nullptr;
-		new_head->ar_p.emplace(new_head->ar_p.begin(), this);
-	}
-	else if (this->get_head_p() != nullptr && new_head == nullptr) {
-		base* root = this;
-		while (root->get_head_p() != nullptr) {
-			 root = root->get_head_p();
-		}
-		this->get_head_p()->ar_p.erase(find(this->get_head_p()->ar_p.begin(), this->get_head_p()->ar_p.end(), this));
-		root->head = this;
-		this->ar_p.emplace(this->ar_p.end(), root);
-		this->head = new_head;
-	}
-	else if((this->get_head_p() == nullptr && !is_in_tre)){
-		this->head = new_head;
-		new_head->ar_p.emplace(new_head->ar_p.end(), this);
-	}
+	head = new_per;
 }
 
 
 base* base::get_head_p()
 {
-	return this->head;
+	return head;
+}
+
+base::~base() {
+	for (int i = 0; i < ar_p.size(); i++) {
+		delete ar_p[i];
+	}
+}
+
+base* base::find_b_n(string fn)
+{
+	if (now->name == fn) return now;
+	else {
+		for (int i = 0; i < now->ar_p.size(); i++) {
+			base* res = now->ar_p[i]->find(fn);
+			if (res != nullptr) {
+				return res;
+			}
+		}
+	}
+	return nullptr;
+}
+
+void base::print_with_stat() {
+	base* now = this;
+	while (now->get_head_p() != nullptr) {
+		now = now->get_head_p();
+	}
+	std::cout << now->get_name();
+	if (now->ar_p.size() != 0) cout << endl;
+	while (now->ar_p.size() != 0) {
+		std::cout << now->name;
+		for (int i = 0; i < now->ar_p.size(); i++) {
+			std::cout << "  " << now->ar_p[i]->get_name();
+		}
+		now = now->ar_p[now->ar_p.size() - 1];
+		if (now->ar_p.size() != 0)
+			std::cout << std::endl;
+	}
 }
--- 4oop/base.h
+++ 4oop/base.h
@@ -7,16 +7,18 @@ class base
 protected:
 	std::string name;
 	base* head;
-	//base* root;
+	int status;
 	std::vector<base*> ar_p;
 public:
 	
-	base(base*, std::string="Def_name");
+	base(base*, std::string = "Def_name", int stat = 0);
 	void set_name(std::string);
 	std::string get_name();
 	void display();
 	void rebase(base* new_per);
 	base* get_head_p();
+	base* find_b_n(std::string fn);
+	void print_with_stat();
 	
 };
 #endif // 
